2b431c122cd854cf2d928796569e9d6625b41848
Erwin Nurhaq <simkalastoforka@gmail.com>
2022-10-23 22:47:23 +0700

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import { trpc } from "~/utils/trpc";
 
 const Home: NextPage = () => {
-  return <div>YX</div>;
+  const { data, isLoading } = trpc.hello.useQuery({ text: 'Erwin!' })
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  return <div>{data?.greeting}</div>;
 };
 
 export default Home;
